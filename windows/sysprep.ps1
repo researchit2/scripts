e0c728df81d7bdf492cb71b3e1f9d0aa405f8985
researchit2 <58023255+researchit2@users.noreply.github.com>
2019-12-07 16:50:54 +0100

    Update sysprep.ps1

--- windows/sysprep.ps1
+++ windows/sysprep.ps1
@@ -2,4 +2,7 @@ Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Install-Module -Name PSWindowsUpdate -Verbose -Confirm:$false -Force
 Get-WindowsUpdate -Install -AcceptAll -Confirm:$false -IgnoreReboot
 
-c:\windows\system32\sysprep\sysprep.exe /generalize /shutdown /oobe
+echo "c:\windows\system32\sysprep\sysprep.exe /generalize /shutdown /oobe" > d:\sysprep.ps1
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce" -Name "Sysprep" -Value 'powershell -windowstyle hidden -Command "& d:\sysprep.ps1"'
+
+
