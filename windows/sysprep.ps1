c50a0e7ed13f2f84e988472df0f8c12bea7891c6
researchit2 <58023255+researchit2@users.noreply.github.com>
2019-12-07 19:18:21 +0100

    Update sysprep.ps1

--- windows/sysprep.ps1
+++ windows/sysprep.ps1
@@ -1,6 +1,6 @@
 # echo "c:\windows\system32\sysprep\sysprep.exe /generalize /shutdown /oobe" > d:\sysprep.ps1
 # Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce" -Name "Sysprep" -Value 'powershell -windowstyle hidden -Command "& d:\sysprep.ps1"'
-echo "c:\windows\system32\sysprep\sysprep.exe /generalize /shutdown /oobe" > d:\sysprep.ps1
+echo "c:\windows\system32\sysprep\sysprep.exe /generalize /shutdown /oobe" > d:\sysprep.cmd
 schtasks.exe /create /tn "sysprep" /ru SYSTEM /Sc ONSTART /tr "d:\sysprep.cmd"
 
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
