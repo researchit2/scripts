d859a107f36ef4fcf52a59287adc01f9c1bc5377
researchit2 <58023255+researchit2@users.noreply.github.com>
2019-12-07 10:53:43 +0100

    Update sysprep.ps1
    Get-WindowsUpdate prereq.

--- windows/sysprep.ps1
+++ windows/sysprep.ps1
@@ -1,3 +1,5 @@
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Install-Module -Name PSWindowsUpdate -Verbose -Confirm:$false -Force
 Get-WindowsUpdate -Install -AcceptAll -Confirm:$false 
 
 c:\windows\system32\sysprep\sysprep.exe /generalize /shutdown /oobe
