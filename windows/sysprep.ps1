5e9045dca61014cbe1f4d5628b748a83379166d9
researchit2 <58023255+researchit2@users.noreply.github.com>
2019-12-07 21:48:19 +0100

    Update sysprep.ps1

--- windows/sysprep.ps1
+++ windows/sysprep.ps1
@@ -1,12 +1,12 @@
 # echo "c:\windows\system32\sysprep\sysprep.exe /generalize /shutdown /oobe" > d:\sysprep.ps1
 # Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce" -Name "Sysprep" -Value 'powershell -windowstyle hidden -Command "& d:\sysprep.ps1"'
 
-#echo "c:\windows\system32\sysprep\sysprep.exe /generalize /shutdown /oobe" > d:\sysprep.cmd
-#schtasks.exe /create /tn "sysprep" /ru SYSTEM /Sc ONSTART /tr "d:\sysprep.cmd"
+echo "c:\windows\system32\sysprep\sysprep.exe /generalize /shutdown /oobe" > d:\sysprep.cmd
+schtasks.exe /create /tn "sysprep" /ru SYSTEM /Sc ONSTART /tr "cmd /c d:\sysprep.cmd"
 
-#Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-#Install-Module -Name PSWindowsUpdate -Verbose -Confirm:$false -Force
-#Get-WindowsUpdate -Install -AcceptAll -Confirm:$false -AutoReboot
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Install-Module -Name PSWindowsUpdate -Verbose -Confirm:$false -Force
+Get-WindowsUpdate -Install -AcceptAll -Confirm:$false -AutoReboot
 
-c:\windows\system32\sysprep\sysprep.exe /generalize /shutdown /oobe
+#c:\windows\system32\sysprep\sysprep.exe /generalize /shutdown /oobe
 
