62e9e10930b594756e5127ba687af24bd512336c
researchit2 <58023255+researchit2@users.noreply.github.com>
2019-12-08 09:47:38 +0100

    Delete update-os.ps1

--- windows/update-os/update-os.ps1
+++
@@ -1,3 +0,0 @@
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-Install-Module -Name PSWindowsUpdate -Verbose -Confirm:$false -Force
-Get-WindowsUpdate -Install -AcceptAll -Confirm:$false -AutoReboot
